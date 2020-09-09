1acb6a0658441dd0a23241a0ecc7d2a207a53273
Michael Böhme <boehme.mic@gmail.com>
2020-09-09 21:09:33 +0200

    added workflow file

---
+++ .github/workflows/download_RKI_COVID19.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests, os, sys
+from datetime import datetime
+
+VERBOSE  = True
+DATAPATH = os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." + os.sep + ".." + os.sep
+DATE_STR = datetime.fromtimestamp(datetime.now().timestamp()).strftime('%Y-%m-%d')
+FILENAME = "RKI_COVID19_{}.csv".format(DATE_STR)
+FULLNAME = DATAPATH + FILENAME
+CSV_URL  = "https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data"
+
+if os.path.isfile(FULLNAME):
+
+    if VERBOSE:
+        print("The file '{}' exists already.".format(FILENAME))
+        
+    sys.exit(0)
+    
+else:
+    
+    if VERBOSE:
+        print("The file '{}' does not exist.".format(FILENAME))
+        
+    headers = { 'Pragma': 'no-cache', 'Cache-Control': 'no-cache' }
+    
+    r = requests.get(CSV_URL, headers=headers, allow_redirects=True, timeout=5.0)
+    if r.status_code != 200:
+        print("Download failed!")
+        sys.exit(1)
+        
+    with open(FULLNAME, 'wb') as df:
+        df.write(r.content)
+        df.close()        
+    
+    command = "(cd {} && git add {} && git commit -m 'added dump {}' && git fetch origin master && git push origin HEAD:master )".format(DATAPATH, FILENAME, DATE_STR)
+    
+    if VERBOSE:
+        print("Executing:\n{}".format(command))
+    
+    os.system(command)
+    sys.exit(0)
