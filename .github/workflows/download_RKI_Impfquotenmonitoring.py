c812a0de4c3b8f17d8fcf2b646b115e3eb247b3f
Michael Böhme <boehme.mic@gmail.com>
2020-12-29 16:57:06 +0100

    added daily scraper for RKI Impfquotenmonitoring

---
+++ .github/workflows/download_RKI_Impfquotenmonitoring.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests, os, sys
+from datetime import datetime
+
+VERBOSE  = True
+DATAPATH = os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." + os.sep + ".." + os.sep
+DATE_STR = datetime.fromtimestamp(datetime.now().timestamp()).strftime('%Y-%m-%d')
+FILENAME = "Impfquotenmonitoring/RKI_COVID19_Impfquotenmonitoring_{}.xlsx".format(DATE_STR)
+FULLNAME = DATAPATH + FILENAME
+CSV_URL  = "https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx?__blob=publicationFile"
+
+if os.path.isfile(FULLNAME):
+
+    if VERBOSE:
+        print("The file '{}' exists already.".format(FILENAME))
+        
+    sys.exit(0)
+    
+else:
+    
+    if VERBOSE:
+        print("The file '{}' does not exist.".format(FILENAME))
+        
+    headers = { 'Pragma': 'no-cache', 'Cache-Control': 'no-cache' }
+    
+    r = requests.get(CSV_URL, headers=headers, allow_redirects=True, timeout=5.0)
+    if r.status_code != 200:
+        print("Download failed!")
+        sys.exit(1)
+        
+    with open(FULLNAME, 'wb') as df:
+        df.write(r.content)
+        df.close()        
---
+++ Impfquotenmonitoring/README.md
@@ -0,0 +1,1 @@
+# RKI Impfquotenmonitoring
