dfc8a13fe2b20c46f2b9ad32f245ea1758e55dda
HrRodan <77460663+HrRodan@users.noreply.github.com>
2021-03-19 20:45:46 +0100

    Added download Testzahl

---
+++ .github/workflows/download_RKI_Testzahl.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests, os
+from datetime import datetime
+
+VERBOSE = True
+DATAPATH = os.path.dirname(os.path.abspath(
+    __file__)) + os.sep + '..' + os.sep + '..' + os.sep + 'Testzahlen' + os.sep + 'raw_data' + os.sep
+DATE_STR = datetime.now().date().strftime('%Y-%m-%d')
+FILENAME = 'Testzahlen-gesamt_{date}.xlsx'.format(date=DATE_STR)
+FULLNAME = DATAPATH + FILENAME
+CSV_URL = "https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Testzahlen-gesamt.xlsx?__blob=publicationFile"
+
+# download latest data
+if os.path.isfile(FULLNAME):
+
+    if VERBOSE:
+        print("The file '{}' exists already.".format(FILENAME))
+
+else:
+
+    if VERBOSE:
+        print("The file '{}' does not exist.".format(FILENAME))
+
+    headers = {'Pragma': 'no-cache', 'Cache-Control': 'no-cache'}
+
+    r = requests.get(CSV_URL, headers=headers, allow_redirects=True, timeout=5.0)
+    if r.status_code != 200:
+        print("Download failed!")
+
+    else:
+
+        with open(FULLNAME, 'wb') as df:
+            df.write(r.content)
+            df.close()
