a36ffa9375f2afd052b4fa165d54440d7767a886
HrRodan <77460663+HrRodan@users.noreply.github.com>
2021-03-21 20:59:31 +0100

    Change RKI to download class

--- .github/workflows/download_RKI_COVID19.py
+++ .github/workflows/download_RKI_COVID19.py
@@ -1,35 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import requests, os, sys
-from datetime import datetime
+from download_pkg import DownloadFile
+import os
 
-VERBOSE  = True
-DATAPATH = os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." + os.sep + ".." + os.sep
-DATE_STR = datetime.fromtimestamp(datetime.now().timestamp()).strftime('%Y-%m-%d')
-FILENAME = "RKI_COVID19_{}.csv".format(DATE_STR)
-FULLNAME = DATAPATH + FILENAME
-CSV_URL  = "https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data"
+data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..')
+filename = "RKI_COVID19.csv"
+url = "https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data"
 
-if os.path.isfile(FULLNAME):
-
-    if VERBOSE:
-        print("The file '{}' exists already.".format(FILENAME))
-        
-    sys.exit(0)
-    
-else:
-    
-    if VERBOSE:
-        print("The file '{}' does not exist.".format(FILENAME))
-        
-    headers = { 'Pragma': 'no-cache', 'Cache-Control': 'no-cache' }
-    
-    r = requests.get(CSV_URL, headers=headers, allow_redirects=True, timeout=5.0)
-    if r.status_code != 200:
-        print("Download failed!")
-        sys.exit(1)
-        
-    with open(FULLNAME, 'wb') as df:
-        df.write(r.content)
-        df.close()        
+a = DownloadFile(url=url, filename=filename, download_path=data_path, compress=True,add_date=True,add_latest=True)
+a.write_file()
