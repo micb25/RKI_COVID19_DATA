15de3007be247cf48f5be0c890766deec0f65630
HrRodan <77460663+HrRodan@users.noreply.github.com>
2021-03-27 21:56:20 +0100

    disable latest

--- .github/workflows/download_RKI_Testzahlen.py
+++ .github/workflows/download_RKI_Testzahlen.py
@@ -8,5 +8,5 @@ data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..',
 filename = 'Testzahlen-gesamt.xlsx'
 url = "https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Testzahlen-gesamt.xlsx?__blob=publicationFile"
 
-a = DownloadFile(url=url, filename=filename, download_path=data_path, compress=False,add_date=True,add_latest=True)
+a = DownloadFile(url=url, filename=filename, download_path=data_path, compress=False,add_date=True,add_latest=False)
 a.write_file()
--- .github/workflows/download_RKI_Todesfaelle.py
+++ .github/workflows/download_RKI_Todesfaelle.py
@@ -8,5 +8,5 @@ data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..',
 filename = 'COVID-19_Todesfaelle.xlsx'
 url = "https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Projekte_RKI/COVID-19_Todesfaelle.xlsx?__blob=publicationFile"
 
-a = DownloadFile(url=url, filename=filename, download_path=data_path, compress=False,add_date=True,add_latest=True)
+a = DownloadFile(url=url, filename=filename, download_path=data_path, compress=False,add_date=True,add_latest=False)
 a.write_file()
