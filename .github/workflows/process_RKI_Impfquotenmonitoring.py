7926e44cf65f02dac79050740637f8c81eac4980
Michael Böhme <boehme.mic@gmail.com>
2021-06-07 18:38:44 +0200

    fixed processing of vaccination statistics and added age-specific statistics for TH

--- .github/workflows/process_RKI_Impfquotenmonitoring.py
+++ .github/workflows/process_RKI_Impfquotenmonitoring.py
@@ -8,6 +8,7 @@ import numpy as np
 
 DATAPATH     = os.path.dirname(os.path.abspath(__file__)) + os.sep + '..' + os.sep + '..' + os.sep + 'Impfquotenmonitoring' + os.sep + 'raw_data' + os.sep
 PARSED_CSV   = '..' + os.sep + 'RKI_COVID19_Impfquotenmonitoring.csv'
+THURINGIA_CSV= '..' + os.sep + 'RKI_COVID19_Impfquotenmonitoring_Thuringia.csv'
 ONE_DAY_IN_S = 86400
 
 states = {
@@ -29,6 +30,48 @@ states = {
     "Sachsen-Anhalt": 15,
     "Thüringen": 16
 }
+                    
+RKI_population = {
+        8: 11100394, 
+        9: 13124737, 
+        11: 3669491, 
+        12: 2521893, 
+        4:   681202, 
+        2:  1847253, 
+        6:  6288080, 
+        13: 1608138, 
+        3:  7993608, 
+        5: 17947221, 
+        7:  4093903, 
+        10:  986887, 
+        14: 4071971, 
+        15: 2194782, 
+        1:  2903773, 
+        16: 2133378,
+        0: 83166711
+}
+
+# Thuringia
+pop_TH_all     = 2133378 / 100
+pop_TH_A00_A17 =  324465 / 100
+pop_TH_A60p    =  730456 / 100
+pop_TH_A18_A59 = pop_TH_all - pop_TH_A00_A17 - pop_TH_A60p
+pop_TH_A00_A59 = pop_TH_all - pop_TH_A60p
+
+th_dtypes = np.dtype([
+    ('Timestamp', int),
+    ('RS', int),
+    ('State', str),
+    ('abs_1st_vac_A00-A17', int),
+    ('abs_1st_vac_A18-A59', int),
+    ('abs_1st_vac_A00-A59', int),
+    ('abs_1st_vac_A60+', int),
+    ('abs_2nd_vac_A00-A17', int),
+    ('abs_2nd_vac_A18-A59', int),
+    ('abs_2nd_vac_A00-A59', int),
+    ('abs_2n_vac_A60+', int)
+])
+df_th = pd.DataFrame( np.empty(0, dtype=th_dtypes) )
 
 isodate_pattern = re.compile(r"([\d]{4})-([\d]{2})-([\d]{2})")
         
@@ -46,6 +89,7 @@ for r, d, f in os.walk(DATAPATH, topdown=True):
                 if (len(pm_date) < 1) or (len(pm_date[0]) < 3):
                     continue
                 date = datetime(year=int(pm_date[0][0]), month=int(pm_date[0][1]), day=int(pm_date[0][2]))
+                ts = date.timestamp()
                 
                 # check for different formats
                 if date < datetime(year=2021, month=1, day=18):
@@ -180,7 +224,7 @@ for r, d, f in os.walk(DATAPATH, topdown=True):
                     df = df.fillna(0)                    
                     df.to_csv(csv_file, sep=',', decimal='.', encoding='utf-8', float_format='%.3f', index=False)
                     
-                else:
+                elif date < datetime(year=2021, month=6, day=7):
                     
                     # third format
                     
@@ -190,6 +234,9 @@ for r, d, f in os.walk(DATAPATH, topdown=True):
                     idx_id = -1
                     idx_state = -1
                     idx_vac_sum = []
+                    
+                    idx_vac_sum_below_60 = []
+                    idx_vac_sum_above_60 = []
 
                     idx_vac_BT_1st = []
                     idx_vac_MO_1st = []
@@ -200,29 +247,9 @@ for r, d, f in os.walk(DATAPATH, topdown=True):
                     idx_vac_inc_1st = []
                     idx_vac_inc_2nd = []
                     
-                    RKI_population = {
-                            8: 11100394, 
-                            9: 13124737, 
-                            11: 3669491, 
-                            12: 2521893, 
-                            4:   681202, 
-                            2:  1847253, 
-                            6:  6288080, 
-                            13: 1608138, 
-                            3:  7993608, 
-                            5: 17947221, 
-                            7:  4093903, 
-                            10:  986887, 
-                            14: 4071971, 
-                            15: 2194782, 
-                            1:  2903773, 
-                            16: 2133378,
-                            0: 83166711
-                    }
-                    
                     locations = ['Impfzentren', 'niedergelassen' ]
                     vacs = ['eine Impfung', 'begonnene Impfserie', 'vollständig geimpft']
-                    types = ['Gesamt', 'Differenz', 'BioNTech', 'Moderna', 'AstraZeneca']
+                    types = ['Gesamt', 'Differenz', 'BioNTech', 'Moderna', 'AstraZeneca', 'Janssen']
                     
                     for i, column in enumerate(df_a.columns):
                         column_str = ''
@@ -259,7 +286,47 @@ for r, d, f in os.walk(DATAPATH, topdown=True):
                                     if (vacs[0] in column_str) or (vacs[1] in column_str):
                                         idx_vac_AZ_1st.append(i)
                     
-                                        # merge the sheets
+                    ############
+                    # Thuringia
+                    ############
+                    if date >= datetime(year=2021, month=4, day=20):
+                        df_c = pd.read_excel(filename, header=[3], sheet_name=1, nrows=18, engine='openpyxl')
+                        df_c = df_c.fillna(0)
+                        col = df_c.columns
+                    
+                        idx_bl = 15 # Thuringia
+                    
+                        vac_first_below_60_a = '<60 Jahre.2'
+                        vac_first_above_60_a = '60+ Jahre.2'
+                        vac_second_below_60_a = '<60 Jahre.3'
+                        vac_second_above_60_a = '60+Jahre'
+                        
+                        vac_first_below_60_b = '<60 Jahre.4'
+                        vac_first_above_60_b = '60+ Jahre.3'
+                        vac_second_below_60_b = '<60 Jahre.5'
+                        vac_second_above_60_b = '60+Jahre.1'
+                        
+                        vac_first_below_60 = df_c.iloc[idx_bl][vac_first_below_60_a] + df_c.iloc[idx_bl][vac_first_below_60_b]
+                        vac_first_above_60 = df_c.iloc[idx_bl][vac_first_above_60_a] + df_c.iloc[idx_bl][vac_first_above_60_b]
+                        vac_second_below_60 = df_c.iloc[idx_bl][vac_second_below_60_a] + df_c.iloc[idx_bl][vac_second_below_60_b]
+                        vac_second_above_60 = df_c.iloc[idx_bl][vac_second_above_60_a] + df_c.iloc[idx_bl][vac_second_above_60_b]
+                    
+                        data_row = {
+                            'Timestamp'          : int(ts),
+                            'RS'                 : df_c.iloc[idx_bl][col[0]],
+                            'State'              : df_c.iloc[idx_bl][col[1]].replace("*", ""),
+                            'abs_1st_vac_A00-A17': -1,
+                            'abs_1st_vac_A18-A59': -1,
+                            'abs_1st_vac_A00-A59': int(vac_first_below_60),
+                            'abs_1st_vac_A60+'   : int(vac_first_above_60),
+                            'abs_2nd_vac_A00-A17': -1,
+                            'abs_2nd_vac_A18-A59': -1,
+                            'abs_2nd_vac_A00-A59': int(vac_second_below_60),
+                            'abs_2n_vac_A60+'    : int(vac_second_above_60)
+                        }                        
+                        df_th = df_th.append(data_row, ignore_index=True)
+                                        
+                    # merge the sheets
                     dtypes = np.dtype([
                         ('RS', int),
                         ('Bundesland', str),
@@ -329,9 +396,122 @@ for r, d, f in os.walk(DATAPATH, topdown=True):
                         
                     df = df.fillna(0)                    
                     df.to_csv(csv_file, sep=',', decimal='.', encoding='utf-8', float_format='%.3f', index=False)
-        
-# merge CSVs
+                    
+                else:
+                    
+                    # fourth format                    
+                    df_a = pd.read_excel(filename, header=[0, 1, 2], sheet_name=2, nrows=18, engine='openpyxl')
+                    df_a = df_a.fillna(0)
+                    
+                    col = df_a.columns
+                    
+                    idx_id = col[0]
+                    idx_state = col[1]
+                    idx_vac_1st_sum = col[2]
+                    idx_vac_1st_diff = col[7]
+                    
+                    idx_vac_2nd_sum = col[8]
+                    idx_vac_2nd_diff = col[13]
+
+                    idx_vac_BT_1st = col[3]
+                    idx_vac_MO_1st = col[4]
+                    idx_vac_AZ_1st = col[5]
+                    idx_vac_JJ_1st = col[6]
+                                     
+                    # merge the sheets
+                    dtypes = np.dtype([
+                        ('RS', int),
+                        ('Bundesland', str),
+                        ('Impfungenkumulativ', int),
+                        ('DifferenzzumVortag', int),
+                        ('Impfungenpro1.000Einwohner', float),
+                        ('IndikationnachAlter', int),
+                        ('BeruflicheIndikation', int),
+                        ('MedizinischeIndikation', int),
+                        ('PflegeheimbewohnerIn', int),
+                        ('ImpfungenkumulativBiontec', int),
+                        ('ImpfungenkumulativModerna', int),
+                        ('ZweiteImpfungkumulativ', int),
+                        ('ZweiteImpfungDifferenzzumVortag', int)
+                    ])
+                    
+                    df = pd.DataFrame( np.empty(0, dtype=dtypes) )                    
+                    
+                    for i, row in df_a.iterrows():
+                                                
+                        # skip other lines
+                        if 'Bund' in row[idx_state]:
+                            continue
+                        
+                        data_row['RS'] = int(row[idx_id])
+                        data_row['Bundesland'] = row[idx_state].replace('*', '')
+                        data_row['Impfungenkumulativ'] = int(row[idx_vac_1st_sum])
+                        data_row['Impfungenpro1.000Einwohner'] = data_row['Impfungenkumulativ'] / RKI_population[row[idx_id]] * 1000.0
+                        data_row['ZweiteImpfungkumulativ'] = int(row[idx_vac_2nd_sum])
+                        data_row['DifferenzzumVortag'] = int(row[idx_vac_1st_diff])
+                        data_row['ZweiteImpfungDifferenzzumVortag'] = int(row[idx_vac_2nd_diff])
+                        
+                        data_row['ImpfungenkumulativBiontec'] = int(row[idx_vac_BT_1st])
+                        data_row['ImpfungenkumulativModerna'] = int(row[idx_vac_MO_1st])
+                        
+                        # removed data types
+                        data_row['IndikationnachAlter'] = -1
+                        data_row['BeruflicheIndikation'] = -1
+                        data_row['MedizinischeIndikation'] = -1
+                        data_row['PflegeheimbewohnerIn'] = -1
+                        
+                        df = df.append(data_row, ignore_index=True)
+                        
+                    df = df.fillna(0)                    
+                    df.to_csv(csv_file, sep=',', decimal='.', encoding='utf-8', float_format='%.3f', index=False)
+                    
+                    ##############
+                    # Thuringia
+                    ##############
+                    df_c = pd.read_excel(filename, header=[0,1,2], sheet_name=1, nrows=18, engine='openpyxl')
+                    df_c = df_c.fillna(0)                    
+                    col = df_c.columns                
+                    idx_bl = 15
                 
+                    idx_1st_vac_below_18   = col[6]
+                    idx_1st_vac_below_60   = col[7]
+                    idx_1st_vac_above_60   = col[8]
+                    
+                    idx_2nd_vac_below_18   = col[10]
+                    idx_2nd_vac_below_60   = col[11]
+                    idx_2nd_vac_above_60   = col[12]
+                    
+                    num_1st_vac_A00_A17   = int(pop_TH_A00_A17 * df_c.iloc[idx_bl][idx_1st_vac_below_18])
+                    num_1st_vac_A18_A59   = int(pop_TH_A18_A59 * df_c.iloc[idx_bl][idx_1st_vac_below_60])
+                    num_1st_vac_A00_A59   = num_1st_vac_A00_A17 + num_1st_vac_A18_A59
+                    num_1st_vac_A60p      = int(pop_TH_A60p * df_c.iloc[idx_bl][idx_1st_vac_above_60])
+                    
+                    num_2nd_vac_A00_A17   = int(pop_TH_A00_A17 * df_c.iloc[idx_bl][idx_2nd_vac_below_18])
+                    num_2nd_vac_A18_A59   = int(pop_TH_A18_A59 * df_c.iloc[idx_bl][idx_2nd_vac_below_60])
+                    num_2nd_vac_A00_A59   = num_2nd_vac_A00_A17 + num_2nd_vac_A18_A59
+                    num_2nd_vac_A60p      = int(pop_TH_A60p * df_c.iloc[idx_bl][idx_2nd_vac_above_60])
+                    
+                    data_row = {
+                            'Timestamp'          : int(ts),
+                            'RS'                 : df_c.iloc[idx_bl][col[0]],
+                            'State'              : df_c.iloc[idx_bl][col[1]].replace("*", ""),
+                            'abs_1st_vac_A00-A17': num_1st_vac_A00_A17,
+                            'abs_1st_vac_A18-A59': num_1st_vac_A18_A59,
+                            'abs_1st_vac_A00-A59': num_1st_vac_A00_A59,
+                            'abs_1st_vac_A60+'   : num_1st_vac_A60p,
+                            'abs_2nd_vac_A00-A17': num_2nd_vac_A00_A17,
+                            'abs_2nd_vac_A18-A59': num_2nd_vac_A18_A59,
+                            'abs_2nd_vac_A00-A59': num_2nd_vac_A00_A59,
+                            'abs_2n_vac_A60+'    : num_2nd_vac_A60p
+                    }
+                        
+                    df_th = df_th.append(data_row, ignore_index=True)
+            
+# export TH age data
+df_th.sort_values(by=['Timestamp'], ascending=True, axis=0, inplace=True)
+df_th.to_csv(DATAPATH + THURINGIA_CSV, sep=',', decimal=".", encoding='utf-8', float_format='%.0f', index=False)
+            
+# merge CSVs                
 columns = [ 
         'timestamp', 'ISODate', 'IdBundesland', 'Bundesland', 
         'Impfungenkumulativ', 'DifferenzzumVortag', 'IndikationnachAlter', 'BeruflicheIndikation', 'MedizinischeIndikation', 'PflegeheimbewohnerIn',
